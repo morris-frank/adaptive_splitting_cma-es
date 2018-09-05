4217c3ee12fd655e297144441a18a5b7d2f2fc16
Maurice Frank <maurice.frank@posteo.de>
2018-09-05 20:09:00 +0200

    init

---
+++ player0.java
@@ -0,0 +1,65 @@
+import org.vu.contest.ContestSubmission;
+import org.vu.contest.ContestEvaluation;
+
+import java.util.Random;
+import java.util.Properties;
+
+public class player0 implements ContestSubmission
+{
+	Random rnd_;
+	ContestEvaluation evaluation_;
+    private int evaluations_limit_;
+	
+	public player0()
+	{
+		rnd_ = new Random();
+	}
+	
+	public void setSeed(long seed)
+	{
+		// Set seed of algortihms random process
+		rnd_.setSeed(seed);
+	}
+
+	public void setEvaluation(ContestEvaluation evaluation)
+	{
+		// Set evaluation problem used in the run
+		evaluation_ = evaluation;
+		
+		// Get evaluation properties
+		Properties props = evaluation.getProperties();
+        // Get evaluation limit
+        evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
+		// Property keys depend on specific evaluation
+		// E.g. double param = Double.parseDouble(props.getProperty("property_name"));
+        boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
+        boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
+        boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
+
+		// Do sth with property values, e.g. specify relevant settings of your algorithm
+        if(isMultimodal){
+            // Do sth
+        }else{
+            // Do sth else
+        }
+    }
+    
+	public void run()
+	{
+		// Run your algorithm here
+        
+        int evals = 0;
+        // init population
+        // calculate fitness
+        while(evals<evaluations_limit_){
+            // Select parents
+            // Apply crossover / mutation operators
+            double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+            // Check fitness of unknown fuction
+            Double fitness = (double) evaluation_.evaluate(child);
+            evals++;
+            // Select survivors
+        }
+
+	}
+}
