784456e445bf8dcd9ac1002ff4b32f52d90c18e2
Maurice Frank <maurice.frank@posteo.de>
2018-09-17 16:42:56 +0200

    added decompiled code

---
+++ BentCigarFunction.java
@@ -0,0 +1,80 @@
+import java.util.Properties;
+import javabbob.JNIfgeneric;
+import javabbob.JNIfgeneric.Params;
+import org.vu.contest.ContestEvaluation;
+
+
+
+public class BentCigarFunction
+  implements ContestEvaluation
+{
+  private static final int EVALS_LIMIT_ = 10000;
+  private static final int bbobid_ = 12;
+  private static final double BASE_ = 9273454.0D;
+  private JNIfgeneric function_ = null;
+
+  private double best_;
+  private double target_;
+  private int evaluations_;
+  private String multimodal_ = "false";
+  private String regular_ = "false";
+  private String separable_ = "false";
+  private String evals_ = Integer.toString(10000);
+
+  public BentCigarFunction()
+  {
+    best_ = 0.0D;
+
+    evaluations_ = 0;
+
+    function_ = new JNIfgeneric();
+    JNIfgeneric.Params localParams = new JNIfgeneric.Params();
+    algName = "";
+    comments = "";
+    JNIfgeneric.makeBBOBdirs("tmp", true);
+    function_.initBBOB(12, 1, 10, "tmp", localParams);
+    target_ = function_.getFtarget();
+  }
+
+
+
+  public Object evaluate(Object paramObject)
+  {
+    if (!(paramObject instanceof double[])) throw new IllegalArgumentException();
+    double[] arrayOfDouble = (double[])paramObject;
+    if (arrayOfDouble.length != 10) { throw new IllegalArgumentException();
+    }
+    if (evaluations_ > 10000) { return null;
+    }
+    double d1 = (function_.evaluate(arrayOfDouble) - target_) / (9273454.0D - target_);
+    double d2 = 10.0D * Math.exp(-5.0D * d1);
+    if (d2 > 10.0D) d2 = 10.0D; else if (d2 < 0.0D) d2 = 0.0D;
+    if (d2 > best_) best_ = d2;
+    evaluations_ += 1;
+
+    return new Double(d2);
+  }
+
+
+  public Object getData(Object paramObject)
+  {
+    return null;
+  }
+
+
+  public double getFinalResult()
+  {
+    return best_;
+  }
+
+
+  public Properties getProperties()
+  {
+    Properties localProperties = new Properties();
+    localProperties.put("Multimodal", multimodal_);
+    localProperties.put("Regular", regular_);
+    localProperties.put("Separable", separable_);
+    localProperties.put("Evaluations", evals_);
+    return localProperties;
+  }
+}
---
+++ SchaffersEvaluation.java
@@ -0,0 +1,78 @@
+import java.util.Properties;
+import javabbob.JNIfgeneric;
+import javabbob.JNIfgeneric.Params;
+import org.vu.contest.ContestEvaluation;
+
+public class SchaffersEvaluation
+  implements ContestEvaluation
+{
+  private static final int EVALS_LIMIT_ = 100000;
+  private static final int bbobid_ = 17;
+  private static final double BASE_ = -13.35288D;
+  private JNIfgeneric function_ = null;
+  private double best_;
+  private double target_ = 0.0D;
+
+  private int evaluations_;
+  private String multimodal_ = "true";
+  private String regular_ = "true";
+  private String separable_ = "false";
+  private String evals_ = Integer.toString(100000);
+
+  public SchaffersEvaluation()
+  {
+    best_ = 0.0D;
+
+    evaluations_ = 0;
+
+    function_ = new JNIfgeneric();
+    JNIfgeneric.Params localParams = new JNIfgeneric.Params();
+    algName = "";
+    comments = "";
+    JNIfgeneric.makeBBOBdirs("tmp", true);
+    function_.initBBOB(17, 1, 10, "tmp", localParams);
+    target_ = function_.getFtarget();
+  }
+
+
+
+  public Object evaluate(Object paramObject)
+  {
+    if (!(paramObject instanceof double[])) throw new IllegalArgumentException();
+    double[] arrayOfDouble = (double[])paramObject;
+    if (arrayOfDouble.length != 10) { throw new IllegalArgumentException();
+    }
+    if (evaluations_ > 100000) { return null;
+    }
+    double d1 = (function_.evaluate(arrayOfDouble) - target_) / (-13.35288D - target_);
+    double d2 = 10.0D * Math.exp(-5.0D * d1);
+    if (d2 > 10.0D) d2 = 10.0D; else if (d2 < 0.0D) d2 = 0.0D;
+    if (d2 > best_) best_ = d2;
+    evaluations_ += 1;
+
+    return new Double(d2);
+  }
+
+
+  public Object getData(Object paramObject)
+  {
+    return null;
+  }
+
+
+  public double getFinalResult()
+  {
+    return best_;
+  }
+
+
+  public Properties getProperties()
+  {
+    Properties localProperties = new Properties();
+    localProperties.put("Multimodal", multimodal_);
+    localProperties.put("Regular", regular_);
+    localProperties.put("Separable", separable_);
+    localProperties.put("Evaluations", evals_);
+    return localProperties;
+  }
+}
