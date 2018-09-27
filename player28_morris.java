8796bb8f7bb8de96605aba9a8d757d3e84009fd8
Maurice Frank <maurice.frank@posteo.de>
2018-09-27 09:17:34 +0200

    fixed some bugs in morris

--- player28_morris.java
+++ player28_morris.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
+import java.util.ListIterator;
 
 public class player28_morris implements ContestSubmission
 {
@@ -16,9 +17,7 @@ public class player28_morris implements ContestSubmission
     private int evaluations_limit_;
     protected static final double maxPos = 5.0D;
     protected static final int nDim = 10;
-    protected static final int nInd = 100;
     protected static final int nPopulations = 8;
-    double birthrate = 1.0D;
     int evals;
 
     public class Individual implements Comparable<Individual>
@@ -59,24 +58,41 @@ public class player28_morris implements ContestSubmission
         public Vector mean;
         public int generation;
         public double bump = 1.0D;
+        public double[] weights;
+        public double birthrate;
 
-        public Population()
+        public Population(double birthrate)
         {
+            this.birthrate = birthrate;
             size = 0;
             generation = 1;
             mean = new Vector(nDim);
             individuals = new ArrayList<Individual>();
+            genWeights();
+        }
+
+        private void genWeights()
+        {
+            weights = new double[size];
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                weights[i] = size - i + 1;
+                sum += weights[i];
+            }
+            for (int i = 0; i < size; i++)
+                weights[i] /= sum;
         }
 
         public void addRandom(int n)
         {
             for(int i = 0; i < n; i++){
                 Individual individual = new Individual();
-                individual.position = rand(nDim, 0.01);
+                individual.position = rand(nDim, maxPos);
                 individual.fitness();
                 individuals.add(individual);
             }
             size += n;
+            genWeights();
         }
 
         public Matrix positions()
@@ -113,10 +129,10 @@ public class player28_morris implements ContestSubmission
         public void report()
         {
             System.out.print(generation);
-            System.out.print(" | Age: ");
+            System.out.print(" | AVG-Age: ");
             System.out.print(ages().mean());
-            System.out.print(" | Fit: ");
-            System.out.print(fitness().mean());
+            System.out.print(" | MAX-Fit: ");
+            System.out.print(fitness().max());
             System.out.print(" | LR: ");
             System.out.print(bump);
             System.out.println();
@@ -136,6 +152,7 @@ public class player28_morris implements ContestSubmission
                 Individual baby = new Individual();
                 baby.position = sampled_positions.data[i];
                 baby.fitness();
+                if(evals == evaluations_limit_) break;
                 offspring.add(baby);
             }
             return offspring;
@@ -144,32 +161,50 @@ public class player28_morris implements ContestSubmission
         public void selection(int mu)
         {
             // todo Make weights NOT static and resize the indiviual array
-            Vector new_mean = new Vector(nDim);
+
             Collections.sort(individuals);
-            double weight = 1 / (double)mu;
-            for(int i = 0; i < mu; i++)
-                for(int d = 0; d < nDim; d++)
-                    new_mean.data[d] += weight * individuals.get(i).position[d];
             while (individuals.size() > mu)
                 individuals.remove(individuals.size() - 1);
             size = mu;
 
-            if(mean.minus(new_mean).mean() < 10E-8){
-                bump *= 1.5;
-            }else{
-                bump = 1;
+            Vector new_mean = new Vector(nDim);
+
+            //Learning rate
+            double c_m = 0.9;
+
+            for(int d = 0; d < nDim; d++) {
+                for(int i = 0; i < mu; i++)
+                    new_mean.data[d] += weights[i] * (individuals.get(i).position[d] - mean.data[d]);
+                new_mean.data[d] *= c_m;
+                new_mean.data[d] += mean.data[d];
             }
+
+            if(mean.minus(new_mean).mean() < 10E-8) bump *= 1.5;
+            else bump = 1;
             mean = new_mean;
         }
 
-        public void nextGeneration()
+        public void killElderly(int maxAge)
+        {
+            ListIterator<Individual> indiIt = individuals.listIterator();
+            while(indiIt.hasNext()) {
+                Individual individual = indiIt.next();
+                if(individual.age > maxAge) {
+                    indiIt.remove();
+                }
+            }
+            // size = individuals.size();
+        }
+
+        public boolean nextGeneration()
         {
             newYear();
-            report();
             covariance = positions().covariance();
             List<Individual> children = offspring((int)(size * birthrate));
             individuals.addAll(children);
+            // killElderly(5);
             selection(size);
+            return evals < evaluations_limit_;
         }
     }
 
@@ -239,6 +274,16 @@ public class player28_morris implements ContestSubmission
             return mean;
         }
 
+        public double max()
+        {
+            double max = 0;
+            for (int i = 0; i < N; i++)
+                for (int d = 0; d < Dim; d++)
+                    if(data[i][d] > max)
+                        max = data[i][d];
+            return max;
+        }
+
         public Matrix covariance()
         {
             Matrix covariance = new Matrix(Dim, Dim);
@@ -280,7 +325,6 @@ public class player28_morris implements ContestSubmission
             System.out.println();
         }
     }
-
     public class Vector
     {
         public final int length;
@@ -311,6 +355,24 @@ public class player28_morris implements ContestSubmission
             for(int i = 0; i < length; i++)
                 sum += data[i];
             return sum / (double)length;
+        }
+
+        public Vector abs()
+        {
+            Vector result = new Vector(this);
+            for (int i = 0; i < length; i++)
+                if (result.data[i] < 0)
+                    result.data[i] = -result.data[i];
+            return result;
+        }
+
+        public double max()
+        {
+            double max = 0;
+            for (int i = 0; i < length; i++)
+                if (data[i] > max)
+                    max = data[i];
+            return max;
         }
 
         public Vector plus(double scalar)
@@ -392,25 +454,15 @@ public class player28_morris implements ContestSubmission
 
 	public void run()
 	{
-        Population population = new Population();
-        population.addRandom(nInd);
-        population.selection(nInd);
-        // for (int i = 0; i < 3; i++)
-        //     population.nextGeneration();
-        while(true)
-            population.nextGeneration();
+        int population_size = 100;
+        double birthrate = 2D;
 
-        // double[] _mean = {0,1};
-        // double[][] _cov = {{1, 0}, {0, 0.2}};
-        // Vector mean = new Vector(_mean);
-        // Matrix cov = new Matrix(_cov);
-
-        // Matrix samples = sample(mean, cov, 1000);
-
-        // Vector estimated_mean = samples.mean();
-        // Matrix estimated_cov = samples.covariance();
-        // estimated_mean.print();
-        // estimated_cov.print();
+        Population population = new Population(birthrate);
+        population.addRandom(population_size);
+        population.selection(population_size);
+        while(population.nextGeneration()){
+            population.report();
+        }
     }
 
     public Matrix sample(Vector mean, Matrix cov, int n)
