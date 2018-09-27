784594bda41075cd1351148e5589b32ab60b728e
Maurice Frank <maurice.frank@posteo.de>
2018-09-27 09:21:29 +0200

    moved morris code to head

--- player28.java
+++ player28.java
@@ -1,22 +1,426 @@
 import org.vu.contest.ContestSubmission;
 import org.vu.contest.ContestEvaluation;
 
-import java.util.Random;
-import java.util.Properties;
+import java.lang.Object;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.Random;
+import java.util.ListIterator;
 
 public class player28 implements ContestSubmission
 {
-	Random rnd_;
+    Random rnd_;
 	ContestEvaluation evaluation_;
     private int evaluations_limit_;
-    protected static final double spaceBoundary = 5.0D;
+    protected static final double maxPos = 5.0D;
     protected static final int nDim = 10;
-    protected static final int nPop = 100;
+    protected static final int nPopulations = 8;
+    int evals;
+
+    public class Individual implements Comparable<Individual>
+    {
+        public double[] position;
+        public double fitness;
+        public int age;
+
+        public Individual()
+        {
+            position = new double[nDim];
+            fitness = 0;
+            age = 1;
+        }
+
+        public double fitness()
+        {
+            if(fitness == 0){
+                evals++;
+                fitness = (double) evaluation_.evaluate(position);
+            }
+            return fitness;
+        }
+
+        @Override
+        public int compareTo(Individual other) {
+            if(this.fitness < other.fitness) return 1;
+            else if(other.fitness < this.fitness) return -1;
+            return 0;
+        }
+    }
+
+    public class Population
+    {
+        public int size;
+        public List<Individual> individuals;
+        public Matrix covariance;
+        public Vector mean;
+        public int generation;
+        public double bump = 1.0D;
+        public double[] weights;
+        public double birthrate;
+
+        public Population(double birthrate)
+        {
+            this.birthrate = birthrate;
+            size = 0;
+            generation = 1;
+            mean = new Vector(nDim);
+            individuals = new ArrayList<Individual>();
+            genWeights();
+        }
+
+        private void genWeights()
+        {
+            weights = new double[size];
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                weights[i] = size - i + 1;
+                sum += weights[i];
+            }
+            for (int i = 0; i < size; i++)
+                weights[i] /= sum;
+        }
+
+        public void addRandom(int n)
+        {
+            for(int i = 0; i < n; i++){
+                Individual individual = new Individual();
+                individual.position = rand(nDim, maxPos);
+                individual.fitness();
+                individuals.add(individual);
+            }
+            size += n;
+            genWeights();
+        }
+
+        public Matrix positions()
+        {
+            Matrix positions = new Matrix(size, nDim);
+            for(int i = 0; i < size; i++)
+                positions.data[i] = individuals.get(i).position;
+            return positions;
+        }
+
+        public Vector ages()
+        {
+            Vector ages = new Vector(size);
+            for(int i = 0; i < size; i++)
+                ages.data[i] = individuals.get(i).age;
+            return ages;
+        }
+
+        public Vector fitness()
+        {
+            Vector fitness = new Vector(size);
+            for(int i = 0; i < size; i++)
+                fitness.data[i] = individuals.get(i).fitness();
+            return fitness;
+        }
+
+        public void newYear()
+        {
+            generation++;
+            for(int i = 0; i < individuals.size(); i++)
+                individuals.get(i).age++;
+        }
+
+        public void report()
+        {
+            System.out.print(generation);
+            System.out.print(" | AVG-Age: ");
+            System.out.print(ages().mean());
+            System.out.print(" | MAX-Fit: ");
+            System.out.print(fitness().max());
+            System.out.print(" | LR: ");
+            System.out.print(bump);
+            System.out.println();
+        }
+
+        public double sigma()
+        {
+            // todo make step size not static
+            return 1 * bump;
+        }
+
+        public List<Individual> offspring(int n)
+        {
+            List<Individual> offspring = new ArrayList<Individual>();
+            Matrix sampled_positions = sample(mean, covariance, n);
+            for(int i = 0; i < n; i++){
+                Individual baby = new Individual();
+                baby.position = sampled_positions.data[i];
+                baby.fitness();
+                if(evals == evaluations_limit_) break;
+                offspring.add(baby);
+            }
+            return offspring;
+        }
+
+        public void selection(int mu)
+        {
+            // todo Make weights NOT static and resize the indiviual array
+
+            Collections.sort(individuals);
+            while (individuals.size() > mu)
+                individuals.remove(individuals.size() - 1);
+            size = mu;
+
+            Vector new_mean = new Vector(nDim);
+
+            //Learning rate
+            double c_m = 0.9;
+
+            for(int d = 0; d < nDim; d++) {
+                for(int i = 0; i < mu; i++)
+                    new_mean.data[d] += weights[i] * (individuals.get(i).position[d] - mean.data[d]);
+                new_mean.data[d] *= c_m;
+                new_mean.data[d] += mean.data[d];
+            }
+
+            if(mean.minus(new_mean).mean() < 10E-8) bump *= 1.5;
+            else bump = 1;
+            mean = new_mean;
+        }
+
+        public void killElderly(int maxAge)
+        {
+            ListIterator<Individual> indiIt = individuals.listIterator();
+            while(indiIt.hasNext()) {
+                Individual individual = indiIt.next();
+                if(individual.age > maxAge) {
+                    indiIt.remove();
+                }
+            }
+            // size = individuals.size();
+        }
+
+        public boolean nextGeneration()
+        {
+            newYear();
+            covariance = positions().covariance();
+            List<Individual> children = offspring((int)(size * birthrate));
+            individuals.addAll(children);
+            // killElderly(5);
+            selection(size);
+            return evals < evaluations_limit_;
+        }
+    }
+
+    public class Matrix
+    {
+        public final int N;
+        public final int Dim;
+        public final double[][] data;
+
+        public Matrix(int N, int Dim)
+        {
+            this.N = N;
+            this.Dim = Dim;
+            data = new double[N][Dim];
+        }
+
+        public Matrix(double[][] data)
+        {
+            N = data.length;
+            Dim = data[0].length;
+            this.data = new double[N][Dim];
+            for (int i = 0; i < N; i++)
+                for (int d = 0; d < Dim; d++)
+                        this.data[i][d] = data[i][d];
+        }
+
+        private Matrix(Matrix other)
+        {
+            this(other.data);
+        }
+
+        public Matrix plus(Vector vector)
+        {
+            Matrix result = new Matrix(this);
+            for(int i = 0; i < N; i++)
+                for(int d = 0; d < Dim; d++)
+                    result.data[i][d] += vector.data[d];
+            return result;
+        }
+
+        public Matrix times(double scalar)
+        {
+            Matrix result = new Matrix(this);
+            for(int i = 0; i < N; i++)
+                for(int d = 0; d < Dim; d++)
+                    result.data[i][d] *= scalar;
+            return result;
+        }
+
+        public Vector times(Vector vector)
+        {
+            Vector result = new Vector(Dim);
+            for(int i = 0; i < N; i++)
+                for(int d = 0; d < Dim; d++)
+                    result.data[d] += data[i][d] * vector.data[d];
+            return result;
+        }
+
+        public Vector mean()
+        {
+            Vector mean = new Vector(Dim);
+            for(int n = 0; n < N; n++)
+                for(int d = 0; d < Dim; d++)
+                    mean.data[d] += data[n][d];
+            for(int d = 0; d < Dim; d++)
+                mean.data[d] /= N;
+            return mean;
+        }
+
+        public double max()
+        {
+            double max = 0;
+            for (int i = 0; i < N; i++)
+                for (int d = 0; d < Dim; d++)
+                    if(data[i][d] > max)
+                        max = data[i][d];
+            return max;
+        }
+
+        public Matrix covariance()
+        {
+            Matrix covariance = new Matrix(Dim, Dim);
+            Vector mean = mean();
+            for(int i = 0; i < N; i++)
+                for(int d = i; d < Dim; d++){
+                    double c = 0;
+                    for(int h = 0; h < N; h++)
+                        c +=  (data[h][i] - mean.data[i]) * (data[h][d] - mean.data[d]);
+                    c /= N - 1;
+                    covariance.data[i][d] = c;
+                    covariance.data[d][i] = c;
+                }
+            return covariance;
+        }
+
+        public Matrix cholesky()
+        {
+            Matrix L = new Matrix(N, N);
+
+            for (int i = 0; i < N; i++){
+                for (int j = 0; j <= i; j++){
+                    double sum = 0.0;
+                    for (int k = 0; k < j; k++)
+                        sum += L.data[i][k] * L.data[j][k];
+                    if (i == j) L.data[i][i] = Math.sqrt(data[i][i] - sum);
+                    else        L.data[i][j] = 1.0 / L.data[j][j] * (data[i][j] - sum);
+                }
+                if (L.data[i][i] <= 0)
+                    throw new RuntimeException("Matrix not positive definite");
+            }
+            return L;
+        }
+
+        public void print()
+        {
+            for(int i = 0; i < N; i++)
+                System.out.println(Arrays.toString(data[i]));
+            System.out.println();
+        }
+    }
+    public class Vector
+    {
+        public final int length;
+        public final double[] data;
+
+        public Vector(int length)
+        {
+            this.length = length;
+            data = new double[length];
+        }
+
+        public Vector(double[] data)
+        {
+            length = data.length;
+            this.data = new double[length];
+            for (int l = 0; l < length; l++)
+                    this.data[l] = data[l];
+        }
+
+        private Vector(Vector other)
+        {
+            this(other.data);
+        }
+
+        public double mean()
+        {
+            double sum = 0;
+            for(int i = 0; i < length; i++)
+                sum += data[i];
+            return sum / (double)length;
+        }
+
+        public Vector abs()
+        {
+            Vector result = new Vector(this);
+            for (int i = 0; i < length; i++)
+                if (result.data[i] < 0)
+                    result.data[i] = -result.data[i];
+            return result;
+        }
+
+        public double max()
+        {
+            double max = 0;
+            for (int i = 0; i < length; i++)
+                if (data[i] > max)
+                    max = data[i];
+            return max;
+        }
+
+        public Vector plus(double scalar)
+        {
+            Vector result = new Vector(this);
+            for (int i = 0; i < length; i++)
+                result.data[i] += scalar;
+            return result;
+        }
+
+        public Vector plus(Vector other)
+        {
+            Vector result = new Vector(this);
+            for(int i = 0; i < length; i++)
+                result.data[i] += other.data[i];
+            return result;
+        }
+
+        public Vector minus(Vector other)
+        {
+            Vector result = new Vector(this);
+            for(int i = 0; i < length; i++)
+                result.data[i] -= other.data[i];
+            return result;
+        }
+
+        public Vector times(double scalar)
+        {
+            Vector result = new Vector(this);
+            for(int i = 0; i < length; i++)
+                result.data[i] *= scalar;
+            return result;
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < length; i++){
+                System.out.print(data[i]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 
 	public player28()
 	{
-		rnd_ = new Random();
+        rnd_ = new Random();
+        evals = 0;
 	}
 
 	public void setSeed(long seed)
@@ -48,175 +452,43 @@ public class player28 implements ContestSubmission
         }
     }
 
-    public double[] generateRandomGenotype()
-    {
-        double[] genotype = new double[nDim];
-        for(int i = 0; i < genotype.length; i++) {
-            genotype[i] = -spaceBoundary + 2 * spaceBoundary * rnd_.nextDouble();
-        }
-
-        return genotype;
-    }
-
-    public double[][] generateRandomPopulation()
-    {
-        double[][] population = new double[nPop][nDim];
-        for(int i = 0; i < population.length; i++){
-            population[i] = generateRandomGenotype();
-        }
-        return population;
-    }
-
-    public int evaluatePopulation(double[][] population, int evals)
-    {
-        for(double[] individuals : population){
-            Double fitness = (double) evaluation_.evaluate(individuals);
-            evals++;
-            if(evals == evaluations_limit_){
-                break;
-            }
-        }
-        return evals;
-    }
-
 	public void run()
 	{
-        int evals = 0;
+        int population_size = 100;
+        double birthrate = 2D;
 
-        double [][] population = generateRandomPopulation();
-
-        while(evals<evaluations_limit_){
-            evals = evaluatePopulation(population, evals);
-            population = generateRandomPopulation();
+        Population population = new Population(birthrate);
+        population.addRandom(population_size);
+        population.selection(population_size);
+        while(population.nextGeneration()){
+            population.report();
         }
     }
 
-    public double[] zeros(int length)
+    public Matrix sample(Vector mean, Matrix cov, int n)
     {
-        double[] result = new double[length];
-        for(int i = 0; i < length; i++)
-            result[i] = 0.0D;
+        Matrix result = new Matrix(n, cov.N);
+        Matrix L = cov.cholesky();
+        for (int i = 0; i < n; i++) {
+            Vector NI = new Vector(randn(cov.N));
+            result.data[i] = L.times(NI).plus(mean).data;
+        }
         return result;
-    }
-
-    public double[][] zeros(int height, int width)
-    {
-        double[][] matrix = new double[height][width];
-        for(int h = 0; h < height; h++)
-            for(int w = 0; w < width; w++)
-                matrix[h][w] = 0.0D;
-        return matrix;
-
     }
 
     public double[] rand(int length, double boundary)
     {
-        double[] vector = new double[length];
+        double[] result = new double[length];
         for(int i = 0; i < length; i++)
-            vector[i] = -boundary + 2 * boundary * rnd_.nextDouble();
-        return vector;
+        result[i] = -boundary + 2 * boundary * rnd_.nextDouble();
+        return result;
     }
 
-    public double[] norm(int length)
+    public double[] randn(int length)
     {
         double[] result = new double[length];
         for(int i = 0; i < length; i++)
             result[i] = rnd_.nextGaussian();
         return result;
-    }
-
-    public double average(double[] vector)
-    {
-        double sum = 0;
-        for(int i = 0; i < vector.length; i++)
-            sum += vector[i];
-        return sum / (double)vector.length;
-    }
-
-    public double[] mean(double[][] matrix)
-    {
-        int width = matrix[0].length;
-        int height = matrix.length;
-        double[] mean = new double[width];
-        for(int w = 0; w < width; w++)
-            for(int h = 0; h < height; h++)
-                mean[w] += matrix[h][w];
-        for(int w = 0; w < width; w++)
-            mean[w] /= height;
-        return mean;
-    }
-
-    public double[][] covariance(double[][] matrix)
-    {
-        int height = matrix.length;
-        int width = matrix[0].length;
-        double[][] covariance = zeros(width, width);
-        double[] mean = mean(matrix);
-        for(int x = 0; x < width; x++)
-            for(int y = x; y < width; y++){
-                double c = 0;
-                for(int h = 0; h < height; h++)
-                    c +=  (matrix[h][x] - mean[x]) * (matrix[h][y] - mean[y]);
-                c /= height - 1;
-                covariance[x][y] = c;
-                covariance[y][x] = c;
-            }
-
-        return covariance;
-    }
-
-    public double[] plus(double[] left, double[] right)
-    {
-        double[] result = new double[left.length];
-        for(int i = 0; i < left.length; i++)
-            result[i] = left[i] + right[i];
-        return result;
-    }
-
-    public double[][] plus(double[] vector, double[][] matrix)
-    {
-        for(int h = 0; h < matrix.length; h++)
-            for(int w = 0; w < matrix[0].length; w++)
-                matrix[h][w] += vector[w];
-        return matrix;
-    }
-
-    public double[] mult(double[][] matrix, double[] vector)
-    {
-        int width = matrix[0].length;
-        double[] result = zeros(width);
-        for(int h = 0; h < matrix.length; h++)
-            for(int w = 0; w < width; w++)
-                result[w] += matrix[h][w] * vector[w];
-        return result;
-    }
-
-    public double[] mult(double scalar, double[] vector)
-    {
-        for(int i = 0; i < vector.length; i++)
-            vector[i] *= scalar;
-        return vector;
-    }
-
-    public double[][] mult(double scalar, double[][] matrix)
-    {
-        for(int h = 0; h < matrix.length; h++)
-            for(int w = 0; w < matrix[0].length; w++)
-                matrix[h][w] *= scalar;
-        return matrix;
-    }
-
-    public void print(double[] vector)
-    {
-        System.out.println(Arrays.toString(vector));
-    }
-
-    public void print(double[][] matrix)
-    {
-        System.out.println();
-        for(int i = 0; i < matrix.length; i++){
-            print(matrix[i]);
-        }
-        System.out.println();
     }
 }
