f065022c10898419cbba52e8594124b7f59070ad
Maurice Frank <maurice.frank@posteo.de>
2018-10-10 17:27:24 +0200

    added split code

--- player28.java
+++ player28.java
@@ -131,6 +131,24 @@ public class player28 implements ContestSubmission
         return x;
     }
 
+    public double times(double[] v, double[] w)
+    {
+        double x = 0;
+        for (int i = 0; i < v.length; i++)
+            x += v[i] * w[i];
+        return x;
+    }
+
+    public double distance(double[] v, double[] w)
+    {
+        double distance = 0;
+        for (int i = 0; i < v.length; i++) {
+            distance += Math.pow(w[i] - v[i], 2.);
+        }
+        distance = Math.sqrt(distance);
+        return distance;
+    }
+
     public double mean(double[] v)
     {
         double X = 0;
@@ -208,7 +226,6 @@ public class player28 implements ContestSubmission
 
         public double[] pc;
         public double[] ps;
-
         public double sigma;
         public int id;
 
@@ -351,16 +368,20 @@ public class player28 implements ContestSubmission
                 individuals.remove(individuals.size() - 1);
         }
 
+        public void calculateMean()
+        {
+            mean = new double[nDim];
+            for (int i = 0; i < individuals.size(); i++)
+                for (int j = 0; j < nDim; j++)
+                    mean[j] += individuals.get(i).position[j] * weights[i];
+        }
+
         public void adapt()
         {
             double N = (double)nDim;
 
             double[] old_mean = mean;
-
-            mean = new double[nDim];
-            for (int i = 0; i < mu; i++)
-                for (int j = 0; j < nDim; j++)
-                    mean[j] += individuals.get(i).position[j] * weights[i];
+            calculateMean();
 
             // Normalized mean difference
             double[] mean_diff = new double[nDim];
@@ -380,10 +401,6 @@ public class player28 implements ContestSubmission
             // sigma = 4. / (1 + Math.exp(-sigmafac * (sigma*sigmafac - sigma)));
             sigma *= Math.exp(Math.min(1., sigmafac));
 
-            // if(individuals.get(1).fitness == individuals.get((int)(0.7 * mu)).fitness){
-            //     sigma *= Math.exp(0.2 + cn);
-            //     System.out.println("ESCAPING FLAT LANDSCAPES!");
-            // }
 
             // double hsig = (norm(ps) / Math.sqrt(1.-Math.pow(1.-cs, 2.*evals/lambda)) / chiN) < (1.4 + 2./(N+1.)) ? 1. : 0.;
             double hsig = (Math.pow(norm(ps),2.)/N  / (1. - Math.pow(1.-cs, 2.*(double)evals/(double)lambda))) < (2. + 4./(N+1.)) ? 1. : 0.;
@@ -421,62 +438,17 @@ public class player28 implements ContestSubmission
             EigenvalueDecomposition eig = new EigenvalueDecomposition(C);
             V = eig.getV();
             D = eig.getD();
+            calculateInvSqrtC();
+        }
+
+        public void calculateInvSqrtC()
+        {
             double[][] invSTD = new double[nDim][nDim];
             for (int i = 0; i < nDim; i++)
                 invSTD[i][i] = Math.max(0., 1./Math.sqrt(D.get(i,i)));
             invSqrtC = new Matrix(invSTD, nDim, nDim);
             invSqrtC = V.times(invSqrtC.times(V.transpose()));
         }
-
-        // public void split()
-        // {
-        //     if (generation < 10) return;
-
-        //     // Finding biggest eigenvalue and index of eigenvector
-        //     double maxE = 0;
-        //     int maxEID = 0;
-        //     for (int i = 0; i < nDim; i++)
-        //         if (D.get(i,i) > maxE) {
-        //             maxE = D.get(i,i);
-        //             maxEID = i;
-        //         }
-
-        //     // Conditioning of the current covariance matrix
-        //     double condition =  maxE / D.minNonZero();
-
-        //     if (condition < 6) return;
-
-        //     int lambda = individuals.size();
-        //     Population newTribe = new Population();
-
-        //     ListIterator<Individual> indiIt = individuals.listIterator();
-        //     while(indiIt.hasNext()) {
-        //         Individual I = indiIt.next();
-
-        //         double strength = 0;
-        //         for (int i = 0; i < nDim; i++)
-        //             strength += (I.position[i] - mean[i]) * V.get(maxEID, i);
-
-        //         if(strength > 0 || (strength == 0 && rnd_.nextBoolean())) {
-        //             newTribe.individuals.add(I);
-        //             indiIt.remove();
-        //         }
-        //     }
-
-        //     newTribe.resetTo(lambda);
-        //     tribes.add(newTribe);
-        //     resetTo(lambda);
-        // }
-
-        // public void resetTo(int lambda)
-        // {
-        //     generation = 1;
-        //     mean = positions().mean();
-        //     updateCovariance();
-        //     if(individuals.size() < lambda)
-        //         reproduce(lambda - individuals.size());
-        //     genWeights();
-        // }
 
         public void updateCovariance()
         {
@@ -493,6 +465,100 @@ public class player28 implements ContestSubmission
             invSqrtC = V.times(invSqrtC.times(V.transpose()));
         }
 
+        public void split()
+        {
+            if(generation < 30)
+                return;
+
+            // Finding biggest and second biggest eigenvalue and index of eigenvector
+            double maxSigma = 0;
+            double max2Sigma = 0;
+            int maxSigmaI = 0;
+            int max2SigmaI = 0;
+            for (int i = 0; i < nDim; i++)
+                if (D.get(i,i) > maxSigma) {
+                    maxSigma = D.get(i,i);
+                    maxSigmaI = i;
+                }
+            for (int i = 0; i < nDim; i++)
+                if (i != maxSigmaI && D.get(i,i) > max2Sigma) {
+                    max2Sigma = D.get(i,i);
+                    max2SigmaI = i;
+                }
+            double[] maxEV = new double[nDim];
+            for (int i = 0; i < nDim; i++)
+                maxEV[i] = V.get(maxSigmaI, i);
+
+            if(maxSigma / max2Sigma < 7)
+                return;
+
+            System.out.println("SPLITTING THE PEOPLE!!!!");
+            Population spartacus = new Population(lambda, mu);
+
+            ListIterator<Individual> indiviualIter = individuals.listIterator();
+            while(indiviualIter.hasNext()) {
+                Individual I = indiviualIter.next();
+
+                double strength = 0;
+                for (int i = 0; i < nDim; i++)
+                    strength += (I.position[i] - mean[i]) * V.get(maxSigmaI, i);
+
+                if(strength > 0) {
+                    spartacus.individuals.add(I);
+                    indiviualIter.remove();
+                }else if(strength == 0){
+                    indiviualIter.remove();
+                }
+            }
+
+            // Copy data from parent population to new population
+            spartacus.V = V.copy();
+            spartacus.D = D.copy();
+            spartacus.calculateInvSqrtC();
+
+
+            // Reset historic paths
+            spartacus.pc = new double[nDim];
+            spartacus.ps = new double[nDim];
+            pc = new double[nDim];
+            ps = new double[nDim];
+            generation = 0;
+
+            // Reset learning step size
+            spartacus.sigma = 0.3 * maxPos;
+            sigma = 0.3 * maxPos;
+
+            // Recalculate the mean
+            calculateMean();
+            spartacus.calculateMean();
+
+            // Reset eigenvalues and eigenvectors
+            D.set(maxSigmaI, maxSigmaI, 1);
+            spartacus.D.set(maxSigmaI, maxSigmaI, 1);
+            for (int i = 0; i < nDim; i++) {
+                V.set(maxSigmaI, i, 0);
+                spartacus.V.set(maxSigmaI, i, 0);
+            }
+            V.set(maxSigmaI, maxSigmaI, 1);
+            spartacus.V.set(maxSigmaI, maxSigmaI, 1);
+            C = V.times(D.times(V.transpose()));
+            spartacus.C = spartacus.V.times(spartacus.D.times(spartacus.V.transpose()));
+
+            tribes.add(spartacus);
+
+            double minDistance = 10000.;
+            for (int i = 0; i < tribes.size(); i++) {
+                for (int j = 0; j < i; j++) {
+                    minDistance = Math.min(distance(tribes.get(i).mean, tribes.get(j).mean), minDistance);
+                }
+            }
+            System.out.println(minDistance);
+            System.out.println(maxSigmaI);
+            print(mean);
+            print(spartacus.mean);
+            print(maxEV);
+        }
+
         public boolean nextGeneration(int lambda, int mu)
         {
             reproduction();
@@ -503,6 +569,7 @@ public class player28 implements ContestSubmission
             if (verbose)
                 report();
             mature();
+            split();
             if(restart && maxFitness > 7. && max(fitness()) < 0.1){
                 reset();
                 initRandom();
